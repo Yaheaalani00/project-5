1d82dd264a26f9dec9865feb82f14b5901979952
yahea alani <yaheaalani123@hotmail.com>
2024-02-21 17:59:46 +0000

    styled navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import styles from "./App.module.css";
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className={styles.App}>
-        <Button variant="primary">Primary</Button>
+      <NavBar />
     </div>
   );
 }
